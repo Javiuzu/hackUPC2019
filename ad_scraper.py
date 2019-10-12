d0e47f57126dc8ee85ddcc0b5f0e43dca1025fab
Javiuzu <javier.garrido.93@gmail.com>
2019-10-12 19:44:39 +0200

    Final version ad_scraper.py

--- ad_scraper.py
+++ ad_scraper.py
@@ -5,8 +5,34 @@ import time
 from adblockparser import AdblockRules
 
 
-url = 'https://stackoverflow.com/questions/51284071/how-to-get-all-the-link-in-page-using-selenium-python	'
+url = 'https://stackoverflow.com/questions/58356824/optimal-strategy-picking-from-ends-of-array-with-contraints'
 rules_file = open("adblock_rules.txt", "r")
+ads = []
+
+def search_href(driver):
+	elems = driver.find_elements_by_xpath("//*[@href]")
+	for link in elems:
+		if rules.should_block(link.get_attribute("href")): 
+			ads.append(link.get_attribute("href"))
+
+def search_src(driver):
+	elems = driver.find_elements_by_xpath("//*[@src]")
+	for link in elems:
+		if rules.should_block(link.get_attribute("src")): 
+			ads.append(link.get_attribute("src"))	
+
+
+def find_all_iframes(driver):
+    iframes = driver.find_elements_by_xpath("//iframe")
+    for index, iframe in enumerate(iframes):
+        # Your sweet business logic applied to iframe goes here.
+        driver.switch_to.frame(index)
+        find_all_iframes(driver)
+        search_href(driver)	
+        search_src(driver)
+
+        driver.switch_to.parent_frame()
+
 
 
 raw_rule = rules_file.readline()
@@ -22,19 +48,16 @@ rules = AdblockRules(raw_rules)
 driver = webdriver.Firefox()
 driver.get(url)
 body = driver.page_source
-adds = []
 time.sleep(5)
-elems = driver.find_elements_by_xpath("//*[@href]")
-for link in elems:
-	if rules.should_block(link.get_attribute("href")): 
-		adds.append(link.get_attribute("href"))
 
-elems = driver.find_elements_by_xpath("//*[@src]")
-for link in elems:
-	if rules.should_block(link.get_attribute("src")): 
-		adds.append(link.get_attribute("src"))
+search_href(driver)
+search_src(driver)
 
 
-print(adds)
+find_all_iframes(driver)
+
+valid_ads = [elem for elem in ads if elem.endswith(".png" ) or elem.endswith(".gif") or elem.endswith(".jpg") or elem.endswith(".jpeg")]
+
+print(valid_ads)
 
 driver.close()
