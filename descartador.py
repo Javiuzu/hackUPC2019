2dff297c43ed83973e9d099e1ee59ac896eaec36
ismalus <ismael.cama.moumen@gmail.com>
2019-10-13 04:59:40 +0300

    last-resort

--- descartador.py
+++ descartador.py
@@ -1,7 +1,13 @@
+
+censura = {}
+
+
 def main():
   palabrasEscogidas = ["a", "b", "ca", "a", "de", "a", "de", "fas", "sad", "de", "for"]
-  palabrasEscogidas = sorted(palabrasEscogidas)
-  print(palabrasEscogidas)
-  palabrasEscogidas = palabrasEscogidas.erase(' ')
+  palabrasEscogidas.sort()
+  foreach (var word in listB)
+    listA.Remove(word);
+  for x in palabrasEscogidas:
+    print(x)
 
 main()
